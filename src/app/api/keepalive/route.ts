403c2aff03aaf9f4d7ca4e5ec0f0a9148c86df40
oximedic1234 <oximedic1234@users.noreply.github.com>
2026-06-16 22:19:54 -0400

    Corregir importación de Prisma

--- src/app/api/keepalive/route.ts
+++ src/app/api/keepalive/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma'; // O la forma en que estés importando Prisma
+import prisma from '@/lib/prisma';
 
 export async function GET() {
   try {
